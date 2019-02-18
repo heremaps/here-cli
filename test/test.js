687c22f86344a3f36e5b38951f33e0037c394b8e
Dhaneesh <dhaneesh.tb@here.com>
2019-02-18 18:24:22 +0530

    updated tests

--- test/test.js
+++ test/test.js
@@ -40,18 +40,15 @@ describe('Configure', function () {
           action: option,
           description: option,
           alias: option,
-          command: option
+          command: option,
+          version: option,
+          parse: option
         };
       }
       mock('commander', {
-        version: function () {
-          return "1.0.0";
-        },
-        help: function () {
-          return "1.0.0";
-        },
-        parse: function () {
-        },
+        version:option,
+        help: option,
+        parse: option,
         command: option
       }
       );
@@ -398,6 +395,26 @@ describe('Configure', function () {
         assert.fail();
       }
     });
+
+    // it('test here geocoder', async function () {
+    //   this.timeout(10000);
+    //   mock('requestAsync', async function(){
+    //     return {respomse:{},body:{ "Response": { "MetaInfo": { "Timestamp": "2016-02-15T10:33:55.504+0000" }, "View": [ { "_type": "SearchResultsViewType", "ViewId": 0, "Result": [ { "Relevance": 1, "MatchLevel": "houseNumber", "MatchQuality": { "State": 1, "City": 1, "Street": [ 0.9 ], "HouseNumber": 1 }, "MatchType": "pointAddress", "Location": { "LocationId": "NT_nL.dzNwdSJgdcF4U8dYEiC_yADM", "LocationType": "address", "DisplayPosition": { "Latitude": 37.37634, "Longitude": -122.03405 }, "NavigationPosition": [ { "Latitude": 37.37643, "Longitude": -122.03444 } ], "MapView": { "TopLeft": { "Latitude": 37.3774642, "Longitude": -122.0354646 }, "BottomRight": { "Latitude": 37.3752158, "Longitude": -122.0326354 } }, "Address": { "Label": "200 S Mathilda Ave, Sunnyvale, CA 94086, United States", "Country": "USA", "State": "CA", "County": "Santa Clara", "City": "Sunnyvale", "District": "Heritage District", "Street": "S Mathilda Ave", "HouseNumber": "200", "PostalCode": "94086", "AdditionalData": [ { "value": "United States", "key": "CountryName" }, { "value": "California", "key": "StateName" }, { "value": "Santa Clara", "key": "CountyName" }, { "value": "N", "key": "PostalCodeType" } ] } } } ] } ] } }};
+    //   });
+    //   const xyz = rewire('../bin/here-geocode');
+    //   var output = '';
+    //   capcon.startCapture(process.stdout, function (stdout) {
+    //     output += stdout;
+    //   });
+      
+    //   await xyz.__get__('geoCode')("mumbai");
+    //   capcon.stopCapture(process.stdout);     
+    //   if (output.indexOf("uploaded feature count :2, failed feature count :0")!=-1) {
+    //     assert.ok(true, "");
+    //   } else {
+    //     assert.fail();
+    //   }
+    // });
     
 
     after(async function () {
