06fc42711ed37de5ab6ffcd520ab679d055b87cb
Nisar, Naitik <naitik.nisar@here.com>
2019-08-22 20:14:23 +0530

    fixing readToken issue
    fixing readToken issue
    
    Signed-off-by: Nisar, Naitik <naitik.nisar@here.com>

--- src/here-xyz.ts
+++ src/here-xyz.ts
@@ -390,7 +390,7 @@ function getSpaceDataFromXyz(id: string, options: any) {
                 jsonOut.features = features;
                 resolve(jsonOut);
             } catch (error) {
-                console.error(`getting data from XYZ space failed: ${error}`);
+                console.error(`getting data from XYZ space failed: ${JSON.stringify(error)}`);
                 reject(error);
             }
         })();
@@ -545,8 +545,8 @@ program
                 } else {
                     cellSizes.push(2000);
                 }
-                if (!options.latitude) {
-                    options.latitude = await getCentreLatitudeOfSpace(id, options.readToken);
+                if (!options.latitude && !options.readToken) {
+                    options.latitude = await getCentreLatitudeOfSpace(id);
                     if (!options.latitude) {
                         options.latitude = 0;
                     }
