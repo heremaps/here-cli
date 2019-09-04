753fefe26299123490218533ffa0605ec0eaf161
Nisar, Naitik <naitik.nisar@here.com>
2019-09-04 20:51:16 +0530

    fix log
    fix log
    
    Signed-off-by: Nisar, Naitik <naitik.nisar@here.com>

--- src/hexbin.ts
+++ src/hexbin.ts
@@ -226,7 +226,6 @@ function calculateHexGrids(features:any[], cellSize:number, isAddIds:boolean, gr
 
           if(aggregate){
             if(isNaN(feature.properties[aggregate])){
-                console.error("Property " + aggregate + " is not numeric for feature - " + JSON.stringify(feature));
                 throw new Error("Property " + aggregate + " is not numeric for feature - " + JSON.stringify(feature));
             }
             if(!outGrid.properties.sum){
