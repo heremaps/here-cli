f2df6a17a11fd2118d955099cf8161f70494617a
Nisar, Naitik <naitik.nisar@here.com>
2019-12-04 20:52:45 +0530

    Basic centroid operation using turf
    Basic centroid operation using turf

--- src/here-xyz.ts
+++ src/here-xyz.ts
@@ -34,6 +34,7 @@ import * as transform from "./transformutil";
 import * as fs from "fs";
 import * as tmp from "tmp";
 import * as summary from "./summary";
+import * as turf from "@turf/turf";
 let cq = require("block-queue");
 import { isBoolean } from "util";
 import { ApiError } from "./api-error";
@@ -2765,6 +2766,58 @@ async function searchableConfig(id: string, options: any) {
     }
 }
 
+program
+    .command("gis <id>")
+    .description("{xyz pro} perform gis operations with space data")
+    .option("--centroid", "calculates centroids of space features")
+    .action(function (id, options) {
+        performGisOperation(id, options).catch((error) => {
+            handleError(error, true);
+        });
+    });
+
+async function performGisOperation(id:string, options:any){
+    await common.verifyProBetaLicense();
+    const sourceId = id;
+    options.totalRecords = Number.MAX_SAFE_INTEGER;
+    options.currentHandleOnly = true;
+    options.handle = 0;
+    let cHandle;
+    let centroidFeatures : any[]= [];
+    let featureCount = 0;
+    console.log("Performing GIS operation on the space data");
+    do {
+        let jsonOut = await getSpaceDataFromXyz(id, options);
+        if (jsonOut.features && jsonOut.features.length === 0 && options.handle == 0) {
+            console.log("\nNo features are available to execute GIS operation");
+            process.exit();
+        }
+        cHandle = jsonOut.handle;
+        options.handle = jsonOut.handle;
+        if (jsonOut.features) {
+            const features = jsonOut.features;
+            features.forEach(function (feature: any, i: number){
+                centroidFeatures.push(turf.centroid(feature, feature.properties));
+                process.stdout.write("\rGIS operation done for feature count - " + (featureCount+i));
+            });
+            featureCount += features.length;
+        } else {
+            cHandle = -1;
+        }
+    } while (cHandle >= 0);
+    process.stdout.write("\n");
+
+    if (centroidFeatures.length > 0) {
+        let tmpObj = tmp.fileSync({ mode: 0o644, prefix: 'gis', postfix: '.json' });
+        fs.writeFileSync(tmpObj.name, JSON.stringify({ type: "FeatureCollection", features: centroidFeatures }));
+        options.tags = 'centroid';
+        options.file = tmpObj.name;
+        options.override = true;
+        await uploadToXyzSpace(id, options);
+        console.log("GIS operation completed on space " + sourceId);
+    }
+}
+
 common.validate(
     [
         "list",
@@ -2780,7 +2833,8 @@ common.validate(
         "hexbin",
         "config",
         "vs",
-        "virtualize"
+        "virtualize",
+        "gis"
     ],
     [process.argv[2]],
     program
