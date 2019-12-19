3b2e70dc6335daf3d8c8b3c62801944266444eb1
Nisar, Naitik <naitik.nisar@here.com>
2019-12-19 20:50:22 +0530

    fixing id generation
    fixing id generation

--- src/here-xyz.ts
+++ src/here-xyz.ts
@@ -712,7 +712,7 @@ program
                         options.tags += ',' + sourceId;
                         //}
                         options.file = tmpObj.name;
-                        //options.override = true;
+                        options.override = true;
                         await uploadToXyzSpace(id, options);
 
                         tmpObj = tmp.fileSync({ mode: 0o644, prefix: 'hex', postfix: '.json' });
@@ -726,7 +726,7 @@ program
                         options.tags += ',' + sourceId;
                         //}
                         options.file = tmpObj.name;
-                        //options.override = true;
+                        options.override = true;
                         await uploadToXyzSpace(id, options);
                         //});
                     } else {
@@ -2946,6 +2946,7 @@ async function performGisOperation(id:string, options:any){
             options.tags = 'voronoi';
         }
         options.file = tmpObj.name;
+        options.override = true;
         await uploadToXyzSpace(id, options);
         console.log("GIS operation completed on space " + sourceId);
     }
